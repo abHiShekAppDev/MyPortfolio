030f654abde0af507f9b51252fd46819a52f266a
abHishekCoDeLab <abhishek.appdev@gmail.com>
2018-03-25 23:27:07 +0530

    Adding some more functionality

---
+++ js/design.js
@@ -0,0 +1,35 @@
+function bplCode(){
+    window.open("https://github.com/abHishekCoDeLab/BPL"); 
+}
+
+function helpMeCode(){
+    window.open("https://github.com/abHishekCoDeLab/HelpMe");
+}
+
+function remindMeCode(){
+     window.open("https://github.com/abHishekCoDeLab/RemindMe");
+}
+
+function getMyRepoCode(){
+     window.open("https://github.com/abHishekCoDeLab/GetMyRepo");
+}
+
+
+function helpMeapk(){
+     window.open("https://github.com/abHishekCoDeLab/HelpMe");
+}
+
+function remindMeapk(){
+window.open("https://drive.google.com/file/d/1A95MpviEQdouTywPs7jOSjunRkOsknAu/view?usp=sharing");
+}
+
+function getMyRepoapk(){
+   window.open("https://github.com/abHishekCoDeLab/GetMyRepo");
+}
+
+function bplApk(){ window.open("https://drive.google.com/file/d/1LYQu66D5K3XpOKAb2zwm1hq9zRWQuv5F/view");
+}
+
+function resume(){
+    window.open("https://drive.google.com/file/d/1cLoXY5BbH789jhv06lhPr8cbxH3Q5SLn/view?usp=sharing"); 
+}
